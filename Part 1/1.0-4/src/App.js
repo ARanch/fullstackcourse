ea106a3868405277c798b7a8b21ddc9f00b9f19a
Andreas Ranch <ranch@sowasound.com>
2022-10-13 23:04:58 +0200

    1.7 done

---
+++ Part 1/1.0-4/src/App.js
@@ -0,0 +1,61 @@
+const Header = (props) => {
+  // header component
+  console.log(props)
+  return <h1>{props.text}</h1>
+}
+
+const Content = (props) => (
+  // Content component
+  // props.map(prop => <Part part={props.part} exercise={props.exercise}/>) // kan man måske gøre det iterativt...?
+  <>
+    <Part part={props.parts[0]}/>
+    <Part part={props.parts[1]}/>
+    <Part part={props.parts[2]}/>
+  </>
+)
+
+const Part = (props) => (
+  // Part component
+  <div>
+    {props.part.name} {props.part.exercises}
+  </div>
+)
+
+const Total = (props) => (
+  // total component
+  // <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+  <p>
+    {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+  </p>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+  return (
+    <div>
+      <Header text={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>  
+    </div>
+  )
+}
+
+export default App
---
+++ Part 1/1.6/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const Header = (props) => (
+  <h1>{props.text}</h1>
+)
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
+// total number
+// avg score
+// amount positive
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [history, setHistory] = useState([])
+  const [average, setAverage] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const calcAvg = () => {
+    let sum = 0
+    history.forEach(value => {sum += value})
+    setAverage(sum/history.length)
+  }
+  
+  const addStats = (voteValue) => {
+    setHistory(history.concat(voteValue))
+    setTotal(total+1)
+    calcAvg()
+    setPositive(good/total*100)
+  }
+
+  const goodVote = () => {
+    setGood(good+1)
+    addStats(1)
+  }
+  
+  const badVote = () => {
+    setBad(bad+1)
+    addStats(-1)
+  }
+  
+  const neutralVote = () => {
+    setNeutral(neutral+1)
+    addStats(0)
+  }
+
+  return (
+    <div>
+      <Header text="Give Feedback"/>
+      <Button text="Good" onClick={() => goodVote()}/>
+      <Button text="Neutral" onClick={() => neutralVote()}/>
+      <Button text="Bad" onClick={() => badVote()}/>
+      <Header text="Statistics"/>
+      Good: {good}<br/>
+      Neutral: {neutral}<br/>
+      Bad: {bad}<br/>
+      Total votes: {total}<br/>
+      Average score: {average}<br/>
+      Amount positive: {positive} %<br/>
+      History: {history}
+    </div>
+  )
+}
+
+export default App
