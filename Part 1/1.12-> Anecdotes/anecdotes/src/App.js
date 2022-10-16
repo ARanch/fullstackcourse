960ab84835640b5a963650bf4d740d50bd2c2d44
Andreas Ranch <ranch@sowasound.com>
2022-10-16 15:03:18 +0200

    1.14 done – also reafactored to using one useState for whole list of anecdotes, because it was hard to work with the list of objects. deleted some files.

--- Part 1/1.12-> Anecdotes/anecdotes/src/App.js
+++ Part 1/1.12-> Anecdotes/anecdotes/src/App.js
@@ -2,37 +2,21 @@ import { useEffect, useState } from 'react'
 
 const App = () => {
   const anecdotes = [
-    {
-      anecdote: 'If it hurts, do it more often.', 
-      votes: useState(0)
-    },
-    {
-      anecdote: 'Adding manpower to a late software project makes it later!',
-      votes: useState(0)
-    },
-    {
-      anecdote: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-      votes: useState(0)
-    },
-    {
-      anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-      votes: useState(0)
-    },
-    {
-      anecdote: 'Premature optimization is the root of all evil.',
-      votes: useState(0)
-    },
-    {
-      anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-      votes: useState(0)
-    },
-    {
-      anecdote: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-      votes: useState(0)
-    }
+    'If it hurts, do it more often.', 
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  let votearray = [] 
+  let maxVotes = 0
+  let maxIndex = Math.max(...votes)
+  maxIndex = votes.indexOf(maxIndex)
 
   const selectAnecdote = () => {
     let rnd = getRandomInt(0, anecdotes.length-1)
@@ -47,13 +31,19 @@ const App = () => {
   }
 
   const voteAnecdote = () => {
-    let currentVotes = anecdotes[selected].votes[0]
-    let setVoteState = anecdotes[selected].votes[1]
-    setVoteState(currentVotes+1)
+    
+    const newVotes = [...votes]
+    console.log(newVotes, 'new votes array')
+    newVotes[selected] = newVotes[selected]+1
+    console.log(newVotes, 'with added vote')
+    // let currentVotes = anecdotes[selected].votes[0]
+    // let setVoteState = anecdotes[selected].votes[1]
+    setVotes(newVotes)
   }
 
   useEffect(() => {
-    console.log('effect fired')
+    console.log('Max index: ', maxIndex)
+    console.log('max indexed anecdote: ',votes[maxIndex])
   })
 
   return (
@@ -61,8 +51,10 @@ const App = () => {
       <button onClick={() => selectAnecdote()}>Next anecdote</button>
       <button onClick={() => voteAnecdote()}>Vote</button>
       <br/>
-      <h2>{anecdotes[selected].anecdote}</h2> <br/>
-      Votes cast for this anecdote: {anecdotes[selected].votes[0]}
+      <h2>{anecdotes[selected]}</h2> <br/>
+      Votes cast for this anecdote: {votes[selected]} <br/>
+      Anecdote with most votes ({votes[maxIndex]}):
+      <h3>{anecdotes[maxIndex]}</h3>
     </div>
   )
 }
